093f39fbfe4a63ceaaaca36f1c88d568bca2e6cc
amr <amrreda547@gmail.com>
2019-06-10 14:46:22 +0200

    add new test to Agent module

--- spec/AgentSpec.js
+++ spec/AgentSpec.js
@@ -1,9 +1,9 @@
 var Agent = require('../src/Agent');
+var agent = new Agent();
 
 describe("Agent",function () {
-
+    
     it("should be successfuly intialized",function () {
-        agent = new Agent();
         
         expect(agent.rules).toEqual(null);
         expect(agent._config).toEqual(null);
@@ -12,6 +12,23 @@ describe("Agent",function () {
         expect(agent.http).toEqual(null);
         expect(agent.Instrumenter).toEqual(null);
         expect(agent._loadedModules).toEqual({});
+    });
+    
+    it("module should be successfuly started",function () {
+        var key = "testKey";
+        agent.start(key);
+        
+        expect(typeof(agent.rules)).toEqual('object');
+        expect(typeof(agent._config)).toEqual('object');
+        expect(agent._config.appKey).toEqual('testKey');
+        expect(agent._config.interval).toEqual(10000);
+        expect(agent._config.debug).toEqual(false);
+        expect(typeof(agent.http)).toEqual('object');
+        expect(agent.http.api.appKey).toEqual('testKey');
+        expect(typeof(agent.Instrumenter)).toEqual('object');
+        expect(typeof(agent._loadedModules)).toEqual('object');
+        expect(typeof(agent.log)).toEqual('function');
+        expect(typeof(agent._info)).toEqual('object');
     });
 
 });
