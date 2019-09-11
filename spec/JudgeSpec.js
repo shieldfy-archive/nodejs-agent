59916a9523ec32bab904dc1e186453065be270aa
Amr-Reda <amrreda547@gmail.com>
2019-09-12 00:25:58 +0200

    WIP: judge spec [execute] function

--- spec/JudgeSpec.js
+++ spec/JudgeSpec.js
@@ -1,18 +1,56 @@
 var Judge = require('../src/Judge');
+var httpMocks = require('node-mocks-http');
+var Rules = require('../src/rules');
+var Agent = require('../src/Agent');
+var key = "testKey";
+var agent = new Agent();
+agent.start(key);
 
 describe("Judge",function () {
 
-    var judge = new Judge({})
+    var rulesList = [
+        {
+            "id" : "x-1-1-1",
+            "module" : "vulnerable-module",
+            "function" : "test",
+            "version" : "<1.1.1",
+            "param" : {
+                "type" : "pathname",
+                "name" : "*"
+            },
+            "rule" : {
+                "type" : "preg",
+                "match" : "..[\\/\\\\]+"
+            }
+        }
+    ];
+    agent.rules = new Rules(rulesList, agent);
+
     describe(".execute",function () {
+        var judge = new Judge(agent)
+        var request  = httpMocks.createRequest({
+            method: 'GET',
+            url: 'http://www.test.com/../../../'
+        });
 
-        var judge = new Judge({})
+        it("should be execute rules on request",function () {
+            var result = judge.execute(request)
+            expect(result).toEqual(false);
+        })
+
+        it("should catch and report for attack",function () {
+            agent.rules._agent._loadedModules["vulnerable-module"] = "<1.1.1"
+            request.connection = { remoteAddress: '127.0.0.1'}
+            var result = judge.execute(request)
+            expect(result).toEqual(true);
+        })
     });
 
-    describe(".executeMonkey",function () {
+    // describe(".executeMonkey",function () {
 
-    });
+    // });
 
-    describe(".report",function () {
+    // describe(".report",function () {
 
-    });
+    // });
 });
