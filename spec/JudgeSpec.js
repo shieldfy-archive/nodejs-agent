e738eb0b7bc4d9fc7061359c49face58b47104d4
amr <amrreda547@gmail.com>
2019-09-11 17:15:01 +0200

    WIP: Judge spec

---
+++ spec/JudgeSpec.js
@@ -0,0 +1,18 @@
+var Judge = require('../src/Judge');
+
+describe("Judge",function () {
+
+    var judge = new Judge({})
+    describe(".execute",function () {
+
+        var judge = new Judge({})
+    });
+
+    describe(".executeMonkey",function () {
+
+    });
+
+    describe(".report",function () {
+
+    });
+});
