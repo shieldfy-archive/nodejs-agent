256c51bc64d2e537096473916f02651f86fb7f31
Amr-Reda <amrreda547@gmail.com>
2019-09-12 18:57:54 +0200

    fix instrrument spec module

--- spec/InstrumentSpec.js
+++ spec/InstrumentSpec.js
@@ -6,10 +6,13 @@ Instrumenter = new Instrument({
     _loadedModules:{},
     monkeyPatch:new MonkeyPatch({log:new Logger(false)})
 });
+var testModule1 = require('fs')
+var testModule2 = require('url')
+var testModule3 = require('node-mocks-http')
 
 describe("Instrument",function () {
 
-    it("should load the modules successfuly",function () {        
+    it("should load the modules successfuly",function () {       
         expect('fs' in Instrumenter._agent._loadedModules).toEqual(true);
         expect('url' in Instrumenter._agent._loadedModules).toEqual(true);
         expect('node-mocks-http' in Instrumenter._agent._loadedModules).toEqual(true);
